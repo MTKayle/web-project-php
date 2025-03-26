161c74cd95a3b6baf5e07b96f12fcaf89766c4db
MTKayle <tan01204986304@gmail.com>
2025-03-26 18:28:47 +0700

    create connection to db

---
+++ config/Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database{
+    private $host = "localhost";
+    private $dbName = "toys_store";
+    private $userName = "user_project_php";
+    private $password = "";
+    public $connection;
+
+    public function getConnection(){
+        $this->connection = null;
+        try {
+            $this->connection = new PDO("mysql:host=".$this->host.";dbname=".$this->dbName, $this->userName, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+        return $this->connection;
+    }
+}
