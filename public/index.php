8009eb8506cc4ce0fa9604fe00cb56a4131eba5f
tecnaaa <giabao14022005@gmail.com>
2025-03-29 12:26:08 +0700

    sync code footer

--- public/index.php
+++ public/index.php
@@ -6,7 +6,6 @@ include '../view/layout/header.php';
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,6 +15,7 @@ include '../view/layout/header.php';
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <script src="../assets/index.js"></script>
     <script src="../assets/js/auth.js"></script>
+    <link rel="stylesheet" href="../view/css/footerStyle.css">
     <link rel="stylesheet" href="../view/css/headerStyle.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/8a0a2d882c.js" crossorigin="anonymous"></script>
@@ -40,4 +40,7 @@ include '../view/layout/header.php';
         </a>
     </div>
 </body>
+<?php
+include '../view/layout/footer.php';
+?>
 </html>
